17da5267e9921be8bf2380a2a42bef77c4d614b5
Eduardo Correa <eduardo.correa@ipdv.mobi>
2019-04-03 22:47:37 -0300

    git

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   client: 'mysql',
-  connection: process.env.LOCALBASE_URL || 'mysql://root:pretinha@127.0.0.1:3306/sorte',
+  connection: process.env.LOCALBASE_URL || 'mysql://cgxeoqab26wdrhus:yuo1un7729zey5qe@s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zlkhmy15eyxlv6p1',
   //connection: process.env.LOCALBASE_URL || 'mysql://dwyzeebkjglicvc9:oa09o2f2zsj63261@e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/c3zm360vj5ueuzvx',
  /* pool: {
     afterCreate: function (conn, callback) {
